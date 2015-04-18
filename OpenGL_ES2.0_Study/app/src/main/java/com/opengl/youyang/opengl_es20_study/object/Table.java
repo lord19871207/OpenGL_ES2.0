3a1b7f6c142bd842258eb514ba41e5eb3dfda5af
youyang <youyang@ucweb.com>
2015-04-18 20:24:35 +0800

    添加绑定数据的方法

--- OpenGL_ES2.0_Study/app/src/main/java/com/opengl/youyang/opengl_es20_study/object/Table.java
+++ OpenGL_ES2.0_Study/app/src/main/java/com/opengl/youyang/opengl_es20_study/object/Table.java
@@ -1,5 +1,7 @@
 package com.opengl.youyang.opengl_es20_study.object;
 
+import android.opengl.GLES20;
+
 import com.opengl.youyang.opengl_es20_study.Constants;
 import com.opengl.youyang.opengl_es20_study.data.VertexArray;
 import com.opengl.youyang.opengl_es20_study.programs.TextureShaderProgram;
@@ -8,27 +10,33 @@ import com.opengl.youyang.opengl_es20_study.programs.TextureShaderProgram;
  * Created by youyang on 15-4-18.
  */
 public class Table {
-    private static final int POSITION_COMPONENT_COUNT=2;
-    private static final int TEXTURE_COORDINATES_COMPONENT_COUNT=2;
-    private static final int STRIDE=(POSITION_COMPONENT_COUNT+TEXTURE_COORDINATES_COMPONENT_COUNT)* Constants.BYTES_PER_FLOAT;
+    private static final int POSITION_COMPONENT_COUNT = 2;
+    private static final int TEXTURE_COORDINATES_COMPONENT_COUNT = 2;
+    private static final int STRIDE = (POSITION_COMPONENT_COUNT + TEXTURE_COORDINATES_COMPONENT_COUNT) * Constants.BYTES_PER_FLOAT;
     private VertexArray vertexArray;
-    private static final float[] VERTEX_DATA={
+    private static final float[] VERTEX_DATA = {
 
             //order of coordinates :X,Y,S,T
-            0f,0f,0.5f,0.5f,
-            -0.5f,-0.8f,0f,0.9f,
-            0.5f,-0.8f,1f,0.9f,
-            0.5f,0.8f,1f,0.1f,
-            -0.5f,0.8f,0f,0.1f,
-            -0.5f,-0.8f,0f,0.9f
+            0f, 0f, 0.5f, 0.5f,
+            -0.5f, -0.8f, 0f, 0.9f,
+            0.5f, -0.8f, 1f, 0.9f,
+            0.5f, 0.8f, 1f, 0.1f,
+            -0.5f, 0.8f, 0f, 0.1f,
+            -0.5f, -0.8f, 0f, 0.9f
     };
 
-    public Table(){
-        vertexArray=new VertexArray(VERTEX_DATA);
+    public Table() {
+        vertexArray = new VertexArray(VERTEX_DATA);
     }
 
-    private void bindData(TextureShaderProgram textureShaderProgram){
-        vertexArray.setVertexAttribPointer(0,textureShaderProgram.);
+    public void bindData(TextureShaderProgram textureShaderProgram) {
+        vertexArray.setVertexAttribPointer(0, textureShaderProgram.getPositionAttributeLocation(), POSITION_COMPONENT_COUNT, STRIDE);
+        vertexArray.setVertexAttribPointer(POSITION_COMPONENT_COUNT,
+                textureShaderProgram.getTextureCoordinatesAttributeLocation(), TEXTURE_COORDINATES_COMPONENT_COUNT, STRIDE);
+    }
+
+    public void draw(){
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_FAN,0,6);
     }
 
 }
