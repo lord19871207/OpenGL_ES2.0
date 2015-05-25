6d5e387d6053d8419b15f95e7d4a331aef925966
youyang <youyang@ucweb.com>
2015-05-25 23:56:29 +0800

    创建顶点缓冲区对象

---
+++ OpenGL_ES2.0_DeskTop_Study/app/src/main/java/com/opengl/youyang/particles/data/VertexBuffer.java
@@ -0,0 +1,13 @@
+package com.opengl.youyang.particles.data;
+
+/**
+ * Created by youyang on 15-5-25.
+ * 创建顶点缓冲区对象
+ */
+public class VertexBuffer {
+    private final int bufferId;
+
+    public VertexBuffer(float[] vertexData){
+
+    }
+}
