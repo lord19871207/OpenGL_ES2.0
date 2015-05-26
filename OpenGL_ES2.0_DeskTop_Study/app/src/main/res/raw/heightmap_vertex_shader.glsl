57894a21ecdd925c014e3a8dbe35c1051fee89a1
youyang <youyang@ucweb.com>
2015-05-26 21:25:09 +0800

    添加高度图的着色器

---
+++ OpenGL_ES2.0_DeskTop_Study/app/src/main/res/raw/heightmap_vertex_shader.glsl
@@ -0,0 +1,8 @@
+uniform mat4 u_Matrix;
+attribute vec3 a_Position;
+varying vec3 v_Color;
+
+void main() {
+    v_Color=mix(vec3(0.180,0.467,0.153),vec3(0.660,0.670,0.680),a_Position.y);
+    gl_Position=u_Matrix*vec4(a_Position,1.0);
+}
